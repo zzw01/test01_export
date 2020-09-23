30ce1a2564f8c0f2a87cb07ec1b388445e89e6ae
root <1727080961@qq.com>
2020-09-23 12:17:22 +0800

    初始化工程02

---
+++ export_domain/src/main/java/cn/itcast/domain/company/User.java
@@ -0,0 +1,12 @@
+package cn.itcast.domain.company;
+
+/**
+ * @Author ZhiWen
+ * @Date 2020/9/23 12:15
+ * @Version JDK 8.0_251
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("IDEA上进行git服务器第一次修改");
+    }
+}
